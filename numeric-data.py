ff1bee89644e8e52c8e270b8d073f9defd5a4f13
Leucrecia Jeruto <60471945+LeuxJeruto@users.noreply.github.com>
2023-07-03 16:59:37 +0300

    Add files via upload

---
+++ numeric-data.py
@@ -0,0 +1,31 @@
+#task 1
+print("Python has three numeric types: int, float, and complex")
+
+#task 2
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#task 3
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#task 4
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+#task 5
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
